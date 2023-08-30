c3005dddb04141b04aa4336c8d9a2a3fe84ceebb
Revant <revantannam22@gmail.com>
2023-08-30 19:20:50 +0530

    experimental commit

---
+++ lib/pages/name.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class Testing extends StatelessWidget {
+  const Testing({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text('data'),
+        ],
+      ),
+    );
+  }
+}
